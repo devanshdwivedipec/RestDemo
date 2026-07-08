3447b6fcb277b191eaee1c0eb18b8e829b4f5dff
devanshdwivedipec <devanshdwivedi18@gmail.com>
2026-07-08 20:47:21 +0530

    08-07

---
+++ src/main/java/com/RestDemo/RestDemo/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.RestDemo.RestDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// For HTTP request
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+}
